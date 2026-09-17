db360860e48107ec4d250e8b4f658533b6e1d496
molka omrani <molka.omrani@esprit.tn>
2026-09-17 21:09:23 +0100

    Initial commit

---
+++ src/main/java/com/example/autoloc/AutoLocApplication.java
@@ -0,0 +1,13 @@
+package com.example.autoloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoLocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoLocApplication.class, args);
+    }
+
+}
